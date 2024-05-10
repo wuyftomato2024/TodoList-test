836e8a4f4b794a721f47950b13564e1398a41724
TOMATO549 <tomato.202404.wuyf@gmail.com>
2024-05-10 12:53:27 +0900

    updata/1253

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './main/TodoList';
+import { BrowserRouter , Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route exact path="/main" element={<TodoList/>}>
+        </Route>
+      
+       </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/main/TodoList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+const TodoList = () => {
+
+    
+
+    return (
+        <>
+            <input />
+            <button onClick={this.handleBtnClick}>add</button>
+            <ul>
+                <li>Learn react</li>
+                <li>Learn English</li>
+            </ul>
+
+            
+        </>
+
+
+    );
+
+
+
+}
+
+
+
+export default TodoList;
